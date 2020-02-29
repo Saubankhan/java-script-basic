6450fd8f754a279075b08ab93a0ccfb7036c40fd
Saubankhan <rezakauser2017@gmail.com>
2020-03-01 00:45:03 +0530

    basic java script

---
+++ index.js
@@ -0,0 +1,91 @@
+// var a=2;
+// console.log(a);
+// console.log(typeof(a));
+// var b="hello";
+// console.log(b);
+// console.log(typeof(b));
+// var c;
+// console.log(c);
+// // console.log(typeof(c));
+// var d=null;
+// console.log(d);
+// console.log(typeof(d));
+// var e=true;
+// console.log(e);
+// console.log(typeof(e));
+
+//Function
+// function sub(a,b){
+//     var c=a-b;
+//     console.log(c);
+//     return c; 
+// }
+// sub(5,3);
+ 
+ //Conditional statement
+// var a=2;
+// if(a>0){
+//     console.log("a is positive");  
+// }
+// else if(a<0){
+//     console.log("a is negative"); 
+// }
+// else{
+//     console.log("a is zero");  
+// }
+
+// var num=0;
+// switch(num) {
+//     case 0:
+//         console.log("a is zero");
+//     break;
+//     case 1:
+//         console.log("a is positive");
+//     break;
+//     default:
+//         console.log("a is negative");
+//         break;
+// }
+
+//looping statement
+// var n=0;
+// while(n<=3){
+//     console.log(n);
+//     n++;
+// }
+
+// var n=5;
+// for (i = 0; i <= n; i++)
+// {
+//   console.log(i);
+// }
+
+//    var j=0;
+// 	do
+// 	{
+// 		console.log(j);
+        
+//         j=j+1;
+//         // console.log(j);
+//     }
+//     while (j<5)
+//     // console.log(j)  
+    
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
